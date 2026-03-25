9c87bafc791689bd5d02430e9dfe75ce1932655a
Alexander Moufarrige <alexmouf.work@gmail.com>
2026-03-25 16:04:15 +0000

    adding trajectory data loading

--- WormChemotaxis/MyIdea.py
+++ WormChemotaxis/MyIdea.py
@@ -34,11 +34,39 @@ print(f"Loading dataset from: {filepath1} and {filepath2}...")
 # load timeseries data (if present) from both
 
 with h5py.File(filepath1, "r") as f:
+    
+    print("Loaded dataset from:", filepath1)
     print("Keys for datasets:", list(f.keys()))
+    
+    traj_data1 = pd.DataFrame(f["trajectories_data"][:])
+    print(
+            f"Trajectory data loaded with shape: {traj_data1.shape} and columns: {traj_data1.columns.tolist()}"
+        )
+    
     if "timeseries_data" in f:
-        timeseries = pd.DataFrame(f["timeseries_data"][:])
+        timeseries1 = pd.DataFrame(f["timeseries_data"][:])
         print(
-        f"Timeseries data loaded with shape: {timeseries.shape} and columns: {timeseries.columns.tolist()}"
+        f"Timeseries data loaded with shape: {timeseries1.shape} and columns: {timeseries1.columns.tolist()}"
+        )
+        
+with h5py.File(filepath2, "r") as f:
+    
+    print("Loaded dataset from:", filepath2)
+    print("Keys for datasets:", list(f.keys()))
+    
+    traj_data2 = pd.DataFrame(f["trajectories_data"][:])
+    print(
+            f"Trajectory data loaded with shape: {traj_data2.shape} and columns: {traj_data2.columns.tolist()}"
+        )
+    
+    if "timeseries_data" in f:
+        timeseries2 = pd.DataFrame(f["timeseries_data"][:])
+        print(
+        f"Timeseries data loaded with shape: {timeseries2.shape} and columns: {timeseries2.columns.tolist()}"
         )
 
 # plot graph of speeds of worms throughout tracking (x axis: time course (s), y axis: speed (micrometres/s))
+
+plt.figure()
+plt.plot(traj_data1['timestamp_time'],timeseries1['speed'],'r:d',linewidth=2,label='mock')
+plt.show()
