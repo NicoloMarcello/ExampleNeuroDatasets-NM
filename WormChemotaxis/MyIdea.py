4bef05b602f9c152e5593b7ef5966a1f8c4caaf9
Alexander Moufarrige <alexmouf.work@gmail.com>
2026-03-25 14:57:56 +0000

    file opening and error code

---
+++ WormChemotaxis/MyIdea.py
@@ -0,0 +1,10 @@
+# Some ideas...
+
+import h5py
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+
+# Open file
+filepath = "Chemotaxis-Data-and-Analysis/Mock_worms/chemotaxis_mock_24_2_23_08_20240123_144841/metadata_featuresN_oneworm.hdf5"
