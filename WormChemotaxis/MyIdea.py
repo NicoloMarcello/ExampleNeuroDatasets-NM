b02f8267235411acebee9e09d1f940b8d7b8f3a3
Alexander Moufarrige <alexmouf.work@gmail.com>
2026-03-25 17:54:31 +0000

    added 3rd file for plotting

--- WormChemotaxis/MyIdea.py
+++ WormChemotaxis/MyIdea.py
@@ -7,8 +7,9 @@ import matplotlib.pyplot as plt
 import sys
 
 # Open file
-filepath1 = "Chemotaxis-Data-and-Analysis/Mock_worms/chemotaxis_mock_24_2_23_08_20240123_144841/metadata_featuresN_oneworm.hdf5"
+filepath1 = "Chemotaxis-Data-and-Analysis/Mock_worms/chemotaxis_mock_210825_3_20250722_161220/metadata_featuresN_oneworm.hdf5"
 filepath2 = "Chemotaxis-Data-and-Analysis/sexually_conditioned_worms/chemotaxis_sexc_24_1_26_09_20240126_143858/metadata_featuresN_oneworm.hdf5"
+filepath3 = "Chemotaxis-Data-and-Analysis/Aversive_worms/chemotaxis_avsv_24_1_23_01_20240124_140022/metadata_featuresN_oneworm.hdf5"
 
 
 # check if file exists
@@ -28,8 +29,18 @@ elif not os.path.exists(filepath2):
         "    git clone https://github.com/Barrios-Lab/Chemotaxis-Data-and-Analysis.git\n"
     )
     quit()
+elif not os.path.exists(filepath3):
+    print(
+        f"\nFile not found: {filepath3}!\nPlease clone the repository into this folder using:\n"
+    )
+    print(
+        "    git clone https://github.com/Barrios-Lab/Chemotaxis-Data-and-Analysis.git\n"
+    )
+    quit()
+    
+print(f"Loading dataset from: {filepath1} and {filepath2} and {filepath3}...")
 
-print(f"Loading dataset from: {filepath1} and {filepath2}...")
+
 
 # load timeseries data (if present) from both
 
@@ -43,12 +54,6 @@ with h5py.File(filepath1, "r") as f:
             f"Trajectory data loaded with shape: {traj_data1.shape} and columns: {traj_data1.columns.tolist()}"
         )
     
-    if "timeseries_data" in f:
-        timeseries1 = pd.DataFrame(f["timeseries_data"][:])
-        print(
-        f"Timeseries data loaded with shape: {timeseries1.shape} and columns: {timeseries1.columns.tolist()}"
-        )
-        
 with h5py.File(filepath2, "r") as f:
     
     print("Loaded dataset from:", filepath2)
@@ -58,15 +63,27 @@ with h5py.File(filepath2, "r") as f:
     print(
             f"Trajectory data loaded with shape: {traj_data2.shape} and columns: {traj_data2.columns.tolist()}"
         )
-    
-    if "timeseries_data" in f:
-        timeseries2 = pd.DataFrame(f["timeseries_data"][:])
-        print(
-        f"Timeseries data loaded with shape: {timeseries2.shape} and columns: {timeseries2.columns.tolist()}"
-        )
 
 # plot graph of speeds of worms throughout tracking (x axis: time course (s), y axis: speed (micrometres/s))
 
 plt.figure()
-plt.plot(traj_data1['timestamp_time'],timeseries1['speed'],'r:d',linewidth=2,label='mock')
+
+strange_scale = 13 
+
+plt.scatter(
+        [i * strange_scale for i in traj_data1["coord_x"]],
+        [i * strange_scale for i in traj_data1["coord_y"]],
+        s=1,
+        c="red",
+        label=f"Worm centroid trajectory (x{strange_scale})",
+    )
+
+plt.scatter(
+        [i * strange_scale for i in traj_data2["coord_x"]],
+        [i * strange_scale for i in traj_data2["coord_y"]],
+        s=1,
+        c="blue",
+        label=f"Worm centroid trajectory (x{strange_scale})",
+    )
+
 plt.show()
